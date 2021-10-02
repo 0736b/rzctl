df9c5a0b2a05b25728be0dd01b5cb98946056850
Lucas Picchi <picchi.lucas@gmail.com>
2021-10-02 18:37:23 -0300

    Update example

--- rzctl-test/rzctl-test.cpp
+++ rzctl-test/rzctl-test.cpp
@@ -11,10 +11,25 @@ int main() {
 	}
 
 	printf("Press CTRL + C to exit\n");
-
 	while (true) {
-
+		Sleep(1000);
+		printf("Move mouse 10 10\n");
 		rzctl::mouse_move(10, 10);
+		Sleep(1000);
+		printf("Press keyboard key a\n");
+		rzctl::keyboard_input(0x1e, KEYBOARD_DOWN);
+		Sleep(1);
+		rzctl::keyboard_input(0x1e, KEYBOARD_UP);
+		Sleep(1000);
+		printf("Right click\n");
+		rzctl::mouse_click(RIGHT_DOWN);
+		Sleep(1);
+		rzctl::mouse_click(RIGHT_UP);
+		Sleep(1000);
+		printf("Left click\n");
+		rzctl::mouse_click(LEFT_DOWN);
+		Sleep(1);
+		rzctl::mouse_click(LEFT_UP);
 
 		// Sleep is needed to avoid razer service overflowing, which delays all your inputs
 		Sleep(1);
